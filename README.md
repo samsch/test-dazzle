2efded1f2260341da3173257e32ee29fddd64e6f
Samuel Scheiderich <samuel.scheiderich@gmail.com>
2018-09-03 17:18:05 -0400

    Initial commit, dazzle issue 51 repro

---
+++ README.md
@@ -0,0 +1,14 @@
+# Dazzle error repro
+
+Install: `npm i`
+
+Build: `npm run build`
+
+Serve: `npm run serve`
+
+Open browser to served page (default `http://127.0.0.1:8080`)
+
+Error in console:
+```
+TypeError: Super expression must either be null or a function, not undefined
+```
---
+++ webpack.config.js
@@ -0,0 +1,17 @@
+/* eslint-env node */
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  optimization: {
+    minimize: true,
+    namedModules: true,
+  },
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+  },
+};
